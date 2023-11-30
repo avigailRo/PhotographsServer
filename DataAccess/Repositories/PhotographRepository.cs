3f7bb1252dead9b722ced1c49f9701a18ed880a2
avigailRo <avigail4192@gmail.com>
2023-11-30 13:28:08 +0200

    commit1

--- DataAccess/Repositories/PhotographRepository.cs
+++ DataAccess/Repositories/PhotographRepository.cs
@@ -22,6 +22,7 @@ namespace DataAccess.Repositories
         public PhotographRepository(ILogger<PhotographRepository> logger)
         {
             this.logger = logger;
+
             handlingJson();
         }
         private async void handlingJson()
